738d7e4b413591a461713d636766c9bc943d515e
appleucj <58881608+appleucj@users.noreply.github.com>
2020-02-12 16:53:19 -0500

    update

--- controllers/burgerController.js
+++ controllers/burgerController.js
@@ -3,11 +3,11 @@ var express = require("express");
 var router = express.Router();
 
 // Import the model (cat.js) to use its database functions.
-var burger = require("../models/burger.js.js");
+var burger = require("../models/burger.js");
 
 // Create all our routes and set up logic within those routes where required.
-router.get("/", function(req, res) {
-  burger.all(function(data) {
+router.get("/", function (req, res) {
+  burger.all(function (data) {
     var hbsObject = {
       burgers: data
     };
@@ -16,14 +16,14 @@ router.get("/", function(req, res) {
   });
 });
 
-router.post("/api/burgers", function(req, res) {
-  burger.create(["name", "devoured"], [req.body.name, req.body.devoured], function(result) {
+router.post("/api/burgers", function (req, res) {
+  burger.create(["name", "devoured"], [req.body.name, req.body.devoured], function (result) {
     // Send back the ID of the new quote
     res.json({ id: result.insertId });
   });
 });
 
-router.put("/api/burgers/:id", function(req, res) {
+router.put("/api/burgers/:id", function (req, res) {
   var condition = "id = " + req.params.id;
 
   console.log("condition", condition);
@@ -33,7 +33,7 @@ router.put("/api/burgers/:id", function(req, res) {
       devoured: req.body.devoured
     },
     condition,
-    function(result) {
+    function (result) {
       if (result.changedRows === 0) {
         // If no rows were changed, then the ID must not exist, so 404
         return res.status(404).end();
